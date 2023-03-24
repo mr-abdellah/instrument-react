23e59b98ec782127eea19eab849b7928c71d672c
ViF-22 <feofanovaviktoriya@gmail.com>
2023-03-24 14:10:53 +0300

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 import { BrowserRouter } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Pages from "./pages/Pages";
 
 export default function App() {
   return (
     <BrowserRouter>
       <div>
+        <Navbar />
         <Pages />
       </div>
     </BrowserRouter>
